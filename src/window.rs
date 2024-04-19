9c33eb27e220d5d9cba8296bbb37279a4b293ef1
RasmusTufvesson <lars@bizzbook.com>
2024-04-19 18:06:18 +0200

    replace comments

--- src/window.rs
+++ src/window.rs
@@ -7,15 +7,7 @@ use winit::{
 pub fn run() -> Result<(), ()> {
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
     event_loop.set_control_flow(ControlFlow::Poll);
-
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(ControlFlow::Wait);
 
     match event_loop.run(move |event, elwt| {
         match event {
@@ -27,24 +19,15 @@ pub fn run() -> Result<(), ()> {
                 elwt.exit();
             },
             Event::AboutToWait => {
-                // Application update code.
+                // insert app code here
 
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
                 window.request_redraw();
             },
             Event::WindowEvent {
                 event: WindowEvent::RedrawRequested,
                 ..
             } => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
+                // insert graphics code here
             },
             _ => ()
         }
